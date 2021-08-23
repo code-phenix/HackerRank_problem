8bb7db45456d1ceaf922211f8a011d4a68259a05
codepheonix <55018311+code-phenix@users.noreply.github.com>
2021-08-23 20:35:33 +0530

    Create SimpleArraySum.java

---
+++ SimpleArraySum.java
@@ -0,0 +1,10 @@
+public static int simpleArraySum(List<Integer> ar) {
+        int result=0;
+    for(int i=0;i<ar.size();i++){
+        result+=ar.get(i);
+    }
+    return result;
+    }
+    
+    
+    
